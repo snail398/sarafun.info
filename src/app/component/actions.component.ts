3f8fbcbed369f6dc98ca4ba60d808035c9964e77
snail398 <lololo398@yandex.ru>
2019-02-17 12:27:23 +0300

    comment pr

--- src/app/component/actions.component.ts
+++ src/app/component/actions.component.ts
@@ -8,6 +8,8 @@ import {Router} from '@angular/router';
 import { ModalComponent} from '../component/modal.component';
 import { DomSanitizer, SafeResourceUrl, SafeUrl,SafeHtml} from '@angular/platform-browser';
 import * as qrcode from '../js/qrcode';
+
+//comment
 @Component({   
   selector: 'app-content',
   templateUrl:'../html/actions.component.html',
